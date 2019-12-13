1e832c8508dc1d27eccaf28f200006d4091a7269
Piet Groot Kormelink <piet@beleggingsmatch.nl>
2019-12-13 12:17:52 +0100

    trying to get logging including exception visible

--- src/handlers/refresh.js
+++ src/handlers/refresh.js
@@ -3,12 +3,17 @@ const { getCurrentTemperaturInCohilva } = require('../sources/source-weather');
 const { ResponceOk } = require('../lib/api-gateway');
 
 exports.refreshHandler = async event => {
-    const _id = 'currenttempincovilha'; // TODO add sfax in the loop
-    const { stage } = event;
-    console.log('stage:', stage, 'event:', event);
-    let results = [];
-    const data = await getCurrentTemperaturInCohilva();
-    const result = await putEvent(stage, { _id, ...data });
-    results.push(result);
-    return ResponceOk(results);
+    try {
+        console.log('refreshHandler event:', event);
+        const _id = 'currenttempincovilha'; // TODO add sfax in the loop
+        const { stage } = event;
+        console.log('stage:', stage);
+        let results = [];
+        const data = await getCurrentTemperaturInCohilva();
+        const result = await putEvent(stage, { _id, ...data });
+        results.push(result);
+        return ResponceOk(results);
+    } catch (e) {
+        throw new Error(`refreshHandler exception: ${JSON.stringify(e)}`);
+    }
 };
